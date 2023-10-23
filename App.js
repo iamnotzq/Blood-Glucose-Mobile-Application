849b57c179689f3a3e683a83587cf952f04e7c0e
Sean Marinas <seanmarinas@gmail.com>
2023-10-23 20:27:58 +0800

    Clean up App.js

--- App.js
+++ App.js
@@ -1,11 +1,4 @@
-import { StatusBar } from "expo-status-bar";
 import { StyleSheet, Text, View } from "react-native";
-import LoginScreen from "./screens/LoginScreen";
-import WelcomeScreen from "./screens/WelcomeScreen";
-import UserParticularsScreen from "./screens/onboarding/UserParticularsScreen";
-import UserMeasurementsScreen from "./screens/onboarding/UserMeasurementsScreen";
-import CreateAccountScreen from "./screens/CreateAccountScreen";
-import UserDiabetesScreen from "./screens/onboarding/UserDiabetesScreen";
 import StackNavigator from "./navigation/StackNavigator";
 
 export default function App() {
