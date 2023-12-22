4ae06e9beb7658bcd6b52343dcaafd4fb8b6de45
Sean Marinas <seanmarinas@gmail.com>
2023-12-22 17:19:54 +0800

    Implemented profile

---
+++ api/dtos/profileDTOs.ts
@@ -0,0 +1,13 @@
+export interface ProfileScreenAssets {
+  firstName: string;
+  lastName: string;
+  age: number;
+  diabetesType: string;
+  weightKg: number;
+  heightCm: number;
+  caloricGoalKcal: number;
+  hypoMgDl: number;
+  targetLowerMgDl: number;
+  targetUpperMgDl: number;
+  hyperMgDl: number;
+}
