b538d3c233a38e488f8481eb929901cfde96efb3
Sean Marinas <seanmarinas@gmail.com>
2024-01-09 16:36:18 +0800

    Fixed bug

--- screens/bloodGlucose/BloodGlucoseChartScreen.js
+++ screens/bloodGlucose/BloodGlucoseChartScreen.js
@@ -143,7 +143,7 @@ const styles = StyleSheet.create({
   },
   analysisContainer: {
     width: 300,
-    height: 100,
+    height: 150,
     backgroundColor: "#ffffff",
     borderRadius: 16,
     borderWidth: 2,
