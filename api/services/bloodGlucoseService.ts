f40ea6cd843be79fd77457f3a4792e916c2850fe
Sean Marinas <seanmarinas@gmail.com>
2024-01-09 14:21:31 +0800

    Fixed bugs

--- api/services/bloodGlucoseService.ts
+++ api/services/bloodGlucoseService.ts
@@ -3,6 +3,7 @@ import BloodGlucoseEntry, {
 } from "../repositories/models/bloodGlucoseEntry";
 
 import { AddBloodGlucoseEntryRequestBody } from "../routes/models/requests/requestBodies";
+
 import {
   BloodGlucoseChartData,
   GetBloodGlucoseChartDataResponseBody,
@@ -32,11 +33,53 @@ const getDatesInRange = (start: Date, end: Date): Date[] => {
   return dates;
 };
 
-const fetchBloodGlucoseDataByTimestamp = async (
+const fetchDailyBloodGlucoseData = async (
+  id: string
+): Promise<BloodGlucoseChartData[]> => {
+  const query = {
+    userId: id,
+  };
+
+  try {
+    const entries: BloodGlucoseEntryDocument[] = await BloodGlucoseEntry.find(
+      query
+    )
+      .sort({ timestamp: -1 }) // Sort in ascending order based on timestamp
+      .limit(5); // Limit the results to the specified count
+
+    const updatedChartDataArray: BloodGlucoseChartData[] = entries.map(
+      (entry) => {
+        const timeString = new Date(entry.timestamp).toLocaleTimeString(
+          "en-US",
+          {
+            hour: "numeric",
+            minute: "numeric",
+          }
+        );
+
+        return {
+          dayString: timeString,
+          glucoseLevel: entry.glucoseLevel,
+        };
+      }
+    );
+
+    updatedChartDataArray.sort((a, b) => {
+      const timeA = new Date(`2000-01-01 ${a.dayString}`);
+      const timeB = new Date(`2000-01-01 ${b.dayString}`);
+      return timeA.getTime() - timeB.getTime();
+    });
+
+    return updatedChartDataArray;
+  } catch (error: any) {
+    throw new Error(`Error in fetchDailyBloodGlucoseData ${error}`);
+  }
+};
+
+const fetchWeeklyBloodGlucoseData = async (
   id: string,
   startingTimestamp: Date,
-  endingTimestamp: Date,
-  period: "daily" | "weekly" | "monthly"
+  endingTimestamp: Date
 ): Promise<BloodGlucoseChartData[]> => {
   const query = {
     userId: id,
@@ -51,99 +94,116 @@ const fetchBloodGlucoseDataByTimestamp = async (
       query
     );
 
-    const entriesByDateMap = new Map<string, number[]>();
-
-    entries.forEach((entry) => {
-      let entryDate = "";
-
-      if (period === "daily") {
-        entryDate = new Date(entry.timestamp).toLocaleTimeString("en-US", {
-          hour: "numeric",
-          minute: "numeric",
-        });
-      } else if (period === "weekly") {
-        entryDate = new Date(entry.timestamp).toLocaleDateString(undefined, {
-          weekday: "short",
-        });
-      } else if (period === "monthly") {
-        entryDate = new Date(entry.timestamp).toLocaleDateString(undefined, {
-          month: "short",
-        });
-      }
-
-      if (!entriesByDateMap.has(entryDate)) {
-        entriesByDateMap.set(entryDate, [entry.glucoseLevel]);
-      }
-
-      entriesByDateMap.get(entryDate)?.push(entry.glucoseLevel);
-    });
-
-    const chartDataArray: BloodGlucoseChartData[] = Array.from(
-      entriesByDateMap
-    ).map(([date, glucoseLevels]) => {
-      const averageGlucose =
-        glucoseLevels.reduce((sum, level) => sum + level, 0) /
-        glucoseLevels.length;
-      const data: BloodGlucoseChartData = {
-        dayString: date,
-        glucoseLevel: averageGlucose,
-      };
-      return data;
-    });
-
     const allDates = getDatesInRange(startingTimestamp, endingTimestamp);
 
-    if (period === "daily") {
-      // Filter entries for the daily period
-      const dailyEntries = entries
-        .filter(
-          (entry) =>
-            entry.timestamp >= startingTimestamp &&
-            entry.timestamp <= endingTimestamp
-        )
-        .sort((a, b) => b.timestamp.getTime() - a.timestamp.getTime())
-        .slice(0, 5); // Select the 5 most recent entries
+    const updatedChartDataArray = allDates.map((date) => {
+      const dateString = new Date(date).toLocaleDateString(undefined, {
+        weekday: "short",
+      });
 
-      // Update chartDataArray with the filtered and sorted entries
-      allDates.forEach((date, index) => {
-        const timeString = new Date(date).toLocaleTimeString("en-US", {
-          hour: "numeric",
-          minute: "numeric",
-        });
-
-        const correspondingEntry = dailyEntries.find(
-          (entry) =>
-            new Date(entry.timestamp).toLocaleTimeString("en-US", {
-              hour: "numeric",
-              minute: "numeric",
-            }) === timeString
+      const correspondingEntries = entries.filter((entry) => {
+        const entryDateString = new Date(entry.timestamp).toLocaleDateString(
+          undefined,
+          {
+            weekday: "short",
+          }
         );
-
-        if (correspondingEntry) {
-          // If there's a matching entry, use its glucose level
-          chartDataArray[index].dayString = timeString;
-          chartDataArray[index].glucoseLevel = correspondingEntry.glucoseLevel;
-        } else {
-          // If no matching entry, set glucose level to 0
-          chartDataArray[index].dayString = timeString;
-          chartDataArray[index].glucoseLevel = 0;
-        }
+        return dateString === entryDateString;
       });
-    } else {
-      // Update the dayString based on the specified period
-      allDates.forEach((date, index) => {
-        // ... (unchanged)
-      });
-    }
 
-    chartDataArray.sort(
+      const glucoseLevel =
+        correspondingEntries.length > 0
+          ? correspondingEntries.reduce(
+              (sum, entry) => sum + entry.glucoseLevel,
+              0
+            ) / correspondingEntries.length
+          : 0;
+
+      return {
+        dayString: dateString,
+        glucoseLevel,
+      };
+    });
+
+    updatedChartDataArray.sort(
       (a, b) =>
         new Date(a.dayString).getTime() - new Date(b.dayString).getTime()
     );
 
-    return chartDataArray;
+    return updatedChartDataArray;
   } catch (error: any) {
-    throw new Error(`Error in fetchBloodGlucoseDataByTimestamp ${error}`);
+    throw new Error(`Error in fetchWeeklyBloodGlucoseData ${error}`);
+  }
+};
+
+const fetchMonthlyBloodGlucoseData = async (
+  id: string,
+  startingTimestamp: Date,
+  endingTimestamp: Date
+): Promise<BloodGlucoseChartData[]> => {
+  const query = {
+    userId: id,
+    timestamp: {
+      $gte: startingTimestamp,
+      $lte: endingTimestamp,
+    },
+  };
+
+  try {
+    const entries: BloodGlucoseEntryDocument[] = await BloodGlucoseEntry.find(
+      query
+    );
+
+    const monthsArray = [];
+    for (let i = 0; i < 5; i++) {
+      const month = new Date();
+      month.setMonth(month.getMonth() - i);
+      monthsArray.push({
+        dateString: month.toLocaleDateString(undefined, {
+          month: "short",
+        }),
+        glucoseLevels: [],
+      });
+    }
+
+    entries.forEach((entry) => {
+      const entryMonthString = new Date(entry.timestamp).toLocaleDateString(
+        undefined,
+        {
+          month: "short",
+        }
+      );
+
+      const correspondingMonth = monthsArray.find(
+        (month) => month.dateString === entryMonthString
+      );
+
+      if (correspondingMonth) {
+        correspondingMonth.glucoseLevels.push(entry.glucoseLevel);
+      }
+    });
+
+    const updatedChartDataArray: BloodGlucoseChartData[] = monthsArray.map(
+      (month) => {
+        const averageGlucose =
+          month.glucoseLevels.reduce((sum, level) => sum + level, 0) /
+            month.glucoseLevels.length || 0;
+
+        return {
+          dayString: month.dateString,
+          glucoseLevel: averageGlucose,
+        };
+      }
+    );
+
+    updatedChartDataArray.sort(
+      (a, b) =>
+        new Date(a.dayString).getTime() - new Date(b.dayString).getTime()
+    );
+
+    return updatedChartDataArray.reverse();
+  } catch (error: any) {
+    throw new Error(`Error in fetchMonthlyBloodGlucoseData ${error}`);
   }
 };
 
@@ -152,36 +212,26 @@ export const getBloodGlucoseChartData = async (
 ): Promise<GetBloodGlucoseChartDataResponseBody> => {
   const currentTimestamp = new Date();
 
-  // Fetch data for the entire current day up to the current time
   try {
     const dailyStartingTimestamp = new Date(currentTimestamp);
     dailyStartingTimestamp.setHours(0, 0, 0, 0);
     dailyStartingTimestamp.setDate(dailyStartingTimestamp.getDate() - 4);
-    const dailyData = await fetchBloodGlucoseDataByTimestamp(
-      id,
-      dailyStartingTimestamp, // Start of the current day
-      currentTimestamp,
-      "daily"
-    );
+    const dailyData = await fetchDailyBloodGlucoseData(id);
 
-    // Fetch data for the last 5 days (including the current day)
     const weeklyStartingTimestamp = new Date(currentTimestamp);
     weeklyStartingTimestamp.setDate(weeklyStartingTimestamp.getDate() - 4);
-    const weeklyData = await fetchBloodGlucoseDataByTimestamp(
+    const weeklyData = await fetchWeeklyBloodGlucoseData(
       id,
       weeklyStartingTimestamp,
-      currentTimestamp,
-      "weekly"
+      currentTimestamp
     );
 
-    // Fetch data for the last 5 months (including the current month)
     const monthlyStartingTimestamp = new Date(currentTimestamp);
     monthlyStartingTimestamp.setDate(monthlyStartingTimestamp.getMonth() - 4);
-    const monthlyData = await fetchBloodGlucoseDataByTimestamp(
+    const monthlyData = await fetchMonthlyBloodGlucoseData(
       id,
       monthlyStartingTimestamp,
-      currentTimestamp,
-      "monthly"
+      currentTimestamp
     );
 
     return {
